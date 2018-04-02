c5e1cf7f42d50657eb8cf322bc396138e9a0fb32
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-04-02 16:49:31 -0400

    Initial commit

---
+++ Assignment5TivaR/Assignment5Form.Designer.cs
@@ -0,0 +1,407 @@
+﻿namespace Assignment5TivaR
+{
+    partial class frmAssignment5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbCom = new System.Windows.Forms.GroupBox();
+            this.lblComTotal = new System.Windows.Forms.Label();
+            this.grbComCard3 = new System.Windows.Forms.GroupBox();
+            this.grbComCard2 = new System.Windows.Forms.GroupBox();
+            this.grbComCard1 = new System.Windows.Forms.GroupBox();
+            this.btnSubmitName = new System.Windows.Forms.Button();
+            this.grbNameQuest = new System.Windows.Forms.GroupBox();
+            this.txtNameBox = new System.Windows.Forms.TextBox();
+            this.lblNameQuest = new System.Windows.Forms.Label();
+            this.grbPlayer = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grbPlayCard3 = new System.Windows.Forms.GroupBox();
+            this.grbPlayCard2 = new System.Windows.Forms.GroupBox();
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniNewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lbl1ComCard = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lbl2ComCard = new System.Windows.Forms.Label();
+            this.lbl3ComCard = new System.Windows.Forms.Label();
+            this.grbPlayCard1 = new System.Windows.Forms.GroupBox();
+            this.lbl1PlayCard = new System.Windows.Forms.Label();
+            this.lbl2PlayCard = new System.Windows.Forms.Label();
+            this.lbl3PlayCard = new System.Windows.Forms.Label();
+            this.btnPass = new System.Windows.Forms.Button();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.grbCom.SuspendLayout();
+            this.grbComCard3.SuspendLayout();
+            this.grbComCard2.SuspendLayout();
+            this.grbComCard1.SuspendLayout();
+            this.grbNameQuest.SuspendLayout();
+            this.grbPlayer.SuspendLayout();
+            this.grbPlayCard3.SuspendLayout();
+            this.grbPlayCard2.SuspendLayout();
+            this.mnuMenu.SuspendLayout();
+            this.grbPlayCard1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbCom
+            // 
+            this.grbCom.Controls.Add(this.lblComTotal);
+            this.grbCom.Controls.Add(this.grbComCard3);
+            this.grbCom.Controls.Add(this.grbComCard2);
+            this.grbCom.Controls.Add(this.grbComCard1);
+            this.grbCom.Location = new System.Drawing.Point(42, 27);
+            this.grbCom.Name = "grbCom";
+            this.grbCom.Size = new System.Drawing.Size(283, 215);
+            this.grbCom.TabIndex = 2;
+            this.grbCom.TabStop = false;
+            this.grbCom.Text = "Computer";
+            // 
+            // lblComTotal
+            // 
+            this.lblComTotal.AutoSize = true;
+            this.lblComTotal.Location = new System.Drawing.Point(198, 28);
+            this.lblComTotal.Name = "lblComTotal";
+            this.lblComTotal.Size = new System.Drawing.Size(49, 13);
+            this.lblComTotal.TabIndex = 2;
+            this.lblComTotal.Text = "Total = 0";
+            // 
+            // grbComCard3
+            // 
+            this.grbComCard3.Controls.Add(this.lbl3ComCard);
+            this.grbComCard3.Location = new System.Drawing.Point(184, 86);
+            this.grbComCard3.Name = "grbComCard3";
+            this.grbComCard3.Size = new System.Drawing.Size(83, 105);
+            this.grbComCard3.TabIndex = 1;
+            this.grbComCard3.TabStop = false;
+            this.grbComCard3.Text = "Card 3";
+            // 
+            // grbComCard2
+            // 
+            this.grbComCard2.Controls.Add(this.lbl2ComCard);
+            this.grbComCard2.Location = new System.Drawing.Point(95, 86);
+            this.grbComCard2.Name = "grbComCard2";
+            this.grbComCard2.Size = new System.Drawing.Size(83, 105);
+            this.grbComCard2.TabIndex = 1;
+            this.grbComCard2.TabStop = false;
+            this.grbComCard2.Text = "Card 2";
+            // 
+            // grbComCard1
+            // 
+            this.grbComCard1.Controls.Add(this.lbl1ComCard);
+            this.grbComCard1.Location = new System.Drawing.Point(6, 86);
+            this.grbComCard1.Name = "grbComCard1";
+            this.grbComCard1.Size = new System.Drawing.Size(83, 105);
+            this.grbComCard1.TabIndex = 0;
+            this.grbComCard1.TabStop = false;
+            this.grbComCard1.Text = "Card 1";
+            // 
+            // btnSubmitName
+            // 
+            this.btnSubmitName.Location = new System.Drawing.Point(21, 82);
+            this.btnSubmitName.Name = "btnSubmitName";
+            this.btnSubmitName.Size = new System.Drawing.Size(98, 23);
+            this.btnSubmitName.TabIndex = 3;
+            this.btnSubmitName.Text = "Submit Name";
+            this.btnSubmitName.UseVisualStyleBackColor = true;
+            this.btnSubmitName.Click += new System.EventHandler(this.btnSubmitName_Click);
+            // 
+            // grbNameQuest
+            // 
+            this.grbNameQuest.Controls.Add(this.txtNameBox);
+            this.grbNameQuest.Controls.Add(this.btnSubmitName);
+            this.grbNameQuest.Controls.Add(this.lblNameQuest);
+            this.grbNameQuest.Location = new System.Drawing.Point(331, 113);
+            this.grbNameQuest.Name = "grbNameQuest";
+            this.grbNameQuest.Size = new System.Drawing.Size(147, 120);
+            this.grbNameQuest.TabIndex = 4;
+            this.grbNameQuest.TabStop = false;
+            // 
+            // txtNameBox
+            // 
+            this.txtNameBox.Location = new System.Drawing.Point(21, 56);
+            this.txtNameBox.Name = "txtNameBox";
+            this.txtNameBox.Size = new System.Drawing.Size(100, 20);
+            this.txtNameBox.TabIndex = 1;
+            // 
+            // lblNameQuest
+            // 
+            this.lblNameQuest.AutoSize = true;
+            this.lblNameQuest.Location = new System.Drawing.Point(18, 31);
+            this.lblNameQuest.Name = "lblNameQuest";
+            this.lblNameQuest.Size = new System.Drawing.Size(101, 13);
+            this.lblNameQuest.TabIndex = 0;
+            this.lblNameQuest.Text = "What is your name?";
+            // 
+            // grbPlayer
+            // 
+            this.grbPlayer.Controls.Add(this.grbPlayCard1);
+            this.grbPlayer.Controls.Add(this.label1);
+            this.grbPlayer.Controls.Add(this.grbPlayCard3);
+            this.grbPlayer.Controls.Add(this.grbPlayCard2);
+            this.grbPlayer.Location = new System.Drawing.Point(42, 287);
+            this.grbPlayer.Name = "grbPlayer";
+            this.grbPlayer.Size = new System.Drawing.Size(283, 215);
+            this.grbPlayer.TabIndex = 3;
+            this.grbPlayer.TabStop = false;
+            this.grbPlayer.Text = "Player";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(198, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Total = 0";
+            // 
+            // grbPlayCard3
+            // 
+            this.grbPlayCard3.Controls.Add(this.lbl3PlayCard);
+            this.grbPlayCard3.Location = new System.Drawing.Point(184, 86);
+            this.grbPlayCard3.Name = "grbPlayCard3";
+            this.grbPlayCard3.Size = new System.Drawing.Size(83, 105);
+            this.grbPlayCard3.TabIndex = 1;
+            this.grbPlayCard3.TabStop = false;
+            this.grbPlayCard3.Text = "Card 3";
+            // 
+            // grbPlayCard2
+            // 
+            this.grbPlayCard2.Controls.Add(this.lbl2PlayCard);
+            this.grbPlayCard2.Location = new System.Drawing.Point(95, 86);
+            this.grbPlayCard2.Name = "grbPlayCard2";
+            this.grbPlayCard2.Size = new System.Drawing.Size(83, 105);
+            this.grbPlayCard2.TabIndex = 1;
+            this.grbPlayCard2.TabStop = false;
+            this.grbPlayCard2.Text = "Card 2";
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenu.Name = "mnuMenu";
+            this.mnuMenu.Size = new System.Drawing.Size(523, 24);
+            this.mnuMenu.TabIndex = 5;
+            this.mnuMenu.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniNewGame,
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniNewGame
+            // 
+            this.mniNewGame.Name = "mniNewGame";
+            this.mniNewGame.Size = new System.Drawing.Size(180, 22);
+            this.mniNewGame.Text = "New Game";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(180, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // lbl1ComCard
+            // 
+            this.lbl1ComCard.AutoSize = true;
+            this.lbl1ComCard.Location = new System.Drawing.Point(7, 30);
+            this.lbl1ComCard.Name = "lbl1ComCard";
+            this.lbl1ComCard.Size = new System.Drawing.Size(35, 13);
+            this.lbl1ComCard.TabIndex = 0;
+            this.lbl1ComCard.Text = "label2";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(363, 287);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(89, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Enter your Name.";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(366, 321);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 7;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // lbl2ComCard
+            // 
+            this.lbl2ComCard.AutoSize = true;
+            this.lbl2ComCard.Location = new System.Drawing.Point(6, 30);
+            this.lbl2ComCard.Name = "lbl2ComCard";
+            this.lbl2ComCard.Size = new System.Drawing.Size(35, 13);
+            this.lbl2ComCard.TabIndex = 0;
+            this.lbl2ComCard.Text = "label2";
+            // 
+            // lbl3ComCard
+            // 
+            this.lbl3ComCard.AutoSize = true;
+            this.lbl3ComCard.Location = new System.Drawing.Point(6, 30);
+            this.lbl3ComCard.Name = "lbl3ComCard";
+            this.lbl3ComCard.Size = new System.Drawing.Size(35, 13);
+            this.lbl3ComCard.TabIndex = 1;
+            this.lbl3ComCard.Text = "label2";
+            // 
+            // grbPlayCard1
+            // 
+            this.grbPlayCard1.Controls.Add(this.lbl1PlayCard);
+            this.grbPlayCard1.Location = new System.Drawing.Point(6, 86);
+            this.grbPlayCard1.Name = "grbPlayCard1";
+            this.grbPlayCard1.Size = new System.Drawing.Size(83, 105);
+            this.grbPlayCard1.TabIndex = 4;
+            this.grbPlayCard1.TabStop = false;
+            this.grbPlayCard1.Text = "Card 1";
+            // 
+            // lbl1PlayCard
+            // 
+            this.lbl1PlayCard.AutoSize = true;
+            this.lbl1PlayCard.Location = new System.Drawing.Point(7, 30);
+            this.lbl1PlayCard.Name = "lbl1PlayCard";
+            this.lbl1PlayCard.Size = new System.Drawing.Size(35, 13);
+            this.lbl1PlayCard.TabIndex = 0;
+            this.lbl1PlayCard.Text = "label2";
+            // 
+            // lbl2PlayCard
+            // 
+            this.lbl2PlayCard.AutoSize = true;
+            this.lbl2PlayCard.Location = new System.Drawing.Point(6, 30);
+            this.lbl2PlayCard.Name = "lbl2PlayCard";
+            this.lbl2PlayCard.Size = new System.Drawing.Size(35, 13);
+            this.lbl2PlayCard.TabIndex = 1;
+            this.lbl2PlayCard.Text = "label2";
+            // 
+            // lbl3PlayCard
+            // 
+            this.lbl3PlayCard.AutoSize = true;
+            this.lbl3PlayCard.Location = new System.Drawing.Point(6, 30);
+            this.lbl3PlayCard.Name = "lbl3PlayCard";
+            this.lbl3PlayCard.Size = new System.Drawing.Size(35, 13);
+            this.lbl3PlayCard.TabIndex = 2;
+            this.lbl3PlayCard.Text = "label2";
+            // 
+            // btnPass
+            // 
+            this.btnPass.Location = new System.Drawing.Point(366, 350);
+            this.btnPass.Name = "btnPass";
+            this.btnPass.Size = new System.Drawing.Size(75, 23);
+            this.btnPass.TabIndex = 8;
+            this.btnPass.Text = "Pass";
+            this.btnPass.UseVisualStyleBackColor = true;
+            // 
+            // btnHit
+            // 
+            this.btnHit.Location = new System.Drawing.Point(366, 379);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(75, 23);
+            this.btnHit.TabIndex = 9;
+            this.btnHit.Text = "Hit";
+            this.btnHit.UseVisualStyleBackColor = true;
+            // 
+            // frmAssignment5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 514);
+            this.Controls.Add(this.btnHit);
+            this.Controls.Add(this.btnPass);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.grbPlayer);
+            this.Controls.Add(this.grbNameQuest);
+            this.Controls.Add(this.grbCom);
+            this.Controls.Add(this.mnuMenu);
+            this.Name = "frmAssignment5";
+            this.Text = "Assignment 5 (Simplified 21) By Tiva Rait";
+            this.grbCom.ResumeLayout(false);
+            this.grbCom.PerformLayout();
+            this.grbComCard3.ResumeLayout(false);
+            this.grbComCard3.PerformLayout();
+            this.grbComCard2.ResumeLayout(false);
+            this.grbComCard2.PerformLayout();
+            this.grbComCard1.ResumeLayout(false);
+            this.grbComCard1.PerformLayout();
+            this.grbNameQuest.ResumeLayout(false);
+            this.grbNameQuest.PerformLayout();
+            this.grbPlayer.ResumeLayout(false);
+            this.grbPlayer.PerformLayout();
+            this.grbPlayCard3.ResumeLayout(false);
+            this.grbPlayCard3.PerformLayout();
+            this.grbPlayCard2.ResumeLayout(false);
+            this.grbPlayCard2.PerformLayout();
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
+            this.grbPlayCard1.ResumeLayout(false);
+            this.grbPlayCard1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbCom;
+        private System.Windows.Forms.Button btnSubmitName;
+        private System.Windows.Forms.GroupBox grbNameQuest;
+        private System.Windows.Forms.TextBox txtNameBox;
+        private System.Windows.Forms.Label lblNameQuest;
+        private System.Windows.Forms.Label lblComTotal;
+        private System.Windows.Forms.GroupBox grbComCard3;
+        private System.Windows.Forms.GroupBox grbComCard2;
+        private System.Windows.Forms.GroupBox grbComCard1;
+        private System.Windows.Forms.GroupBox grbPlayer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox grbPlayCard3;
+        private System.Windows.Forms.GroupBox grbPlayCard2;
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniNewGame;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Label lbl1ComCard;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label lbl3ComCard;
+        private System.Windows.Forms.Label lbl2ComCard;
+        private System.Windows.Forms.GroupBox grbPlayCard1;
+        private System.Windows.Forms.Label lbl1PlayCard;
+        private System.Windows.Forms.Label lbl3PlayCard;
+        private System.Windows.Forms.Label lbl2PlayCard;
+        private System.Windows.Forms.Button btnPass;
+        private System.Windows.Forms.Button btnHit;
+    }
+}
+
---
+++ Assignment5TivaR/Assignment5Form.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Created by: Tiva Rait
+ * Created on: 02-04-2018
+ * Created for: ICS3U Programming
+ * Assignment #5b - Simplified 21
+ * This program let's you play a simplified version of 21
+*/
+
+namespace Assignment5TivaR
+{
+    public partial class frmAssignment5 : Form
+    {
+        // Defining Constants and variables
+        const int MAX_VALUE = 10;
+        const int MIN_VALUE = 1;
+        string playerName;
+
+        public frmAssignment5()
+        {
+            InitializeComponent();
+
+            //To Disable all of the group boxes until name is submitted
+            grbCom.Enabled = false;
+            grbPlayer.Enabled = false;
+            grbNameQuest.Show();
+
+            // To hide the third card until one is needed
+            grbComCard3.Hide();
+            grbPlayCard3.Hide();
+
+            // To set the status bar
+            lblStatus.Text = ("Enter your name");
+
+            // To Set up Hit, Pass, and play buttons
+            this.btnPlay.Location = new Point(355, 321);
+            this.btnHit.Location = new Point(366, 379);
+
+        }
+
+        private void btnSubmitName_Click(object sender, EventArgs e)
+        {
+            // To Name the Player
+            playerName = Convert.ToString(txtNameBox.Text);
+            grbPlayer.Text = (playerName);
+
+            //To start the rest of the game
+            grbNameQuest.Hide();
+            grbPlayer.Enabled = true;
+
+            //To get player to hit Play button
+            lblStatus.Text = ("Are you ready? Hit the play button to start");
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            // To set up what happens to play button after is it clicked
+            this.btnHit.Location = new Point (355, 321);
+            this.btnPlay.Location = new Point(366, 379);
+        }
+    }
+}
